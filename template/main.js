02e2fe218da16ed2411bdd2b50689ead22ae7665
Camdon Cady <camdon.cady@gmail.com>
2025-12-01 00:13:16 -0500

    Update to work better with node

--- template/main.js
+++ template/main.js
@@ -1,2 +1,6 @@
-var message = "Hello, World!";
-console.log(message);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var process_1 = require("process");
+process_1.stdin.on("data", function (data) {
+    console.log("Hello, ".concat(data.toString().trim(), "!"));
+});
